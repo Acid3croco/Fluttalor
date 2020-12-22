f0b4422ce82e81813047cddd6b3e3d78942b78a5
Acid3croco <yoann.ditte@gmail.com>
2020-12-22 08:01:09 +0100

    feat (contact list): add contacts and badges

--- lib/views/contact_list/contact_list.dart
+++ lib/views/contact_list/contact_list.dart
@@ -1,16 +1,20 @@
-import 'package:fluttalor/views/authentication_views/authentication_view.dart';
+// import 'package:fluttalor/views/authentication_views/authentication_view.dart';
 import 'package:flutter/material.dart';
+
+import 'package:badges/badges.dart';
 
 import 'package:fluttalor/views/contact_handler/contact_handler.dart';
 
 class ContactList extends StatelessWidget {
   static const String id = '/contact_list';
 
+  final List<String> names = <String>['Pierre', 'Michelle', 'Yoann', 'Damz'];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Contact'),
+        title: const Text('Contacts'),
         actions: const <Widget>[
           Padding(
             padding: EdgeInsets.only(right: 20),
@@ -18,21 +22,81 @@ class ContactList extends StatelessWidget {
           )
         ],
       ),
-      body: Center(
-        child: Column(
-          children: <Widget>[
-            const Text('Contact list'),
-            ElevatedButton(
-              onPressed: () =>
-                  Navigator.pushNamed(context, AuthenticationView.id),
-              child: const Text('Return to auth view'),
-            )
-          ],
-        ),
+      body: ListView.builder(
+        itemCount: names.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Container(
+            decoration: BoxDecoration(
+              border: Border(
+                bottom: BorderSide(
+                    color: index < names.length - 1
+                        ? Colors.black12
+                        : Colors.transparent),
+              ),
+            ),
+            child: ListTile(
+              contentPadding:
+                  const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+              leading: const CircleAvatar(
+                radius: 30,
+              ),
+              title: Padding(
+                padding: const EdgeInsets.only(bottom: 5),
+                child: Text(
+                  names[index],
+                  style: const TextStyle(fontWeight: FontWeight.w500),
+                ),
+              ),
+              subtitle: Row(
+                children: const <Widget>[
+                  ContactBadge(name: 'AMI'),
+                  ContactBadge(name: 'AMI'),
+                  ContactBadge(name: 'AMI'),
+                ],
+              ),
+            ),
+          );
+        },
+        // separatorBuilder: (BuildContext context, int index) {
+        //   return const Divider(
+        //     thickness: 1,
+        //   );
+        // },
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () => Navigator.pushNamed(context, ContactHandler.id),
         child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class ContactBadge extends StatelessWidget {
+  const ContactBadge({
+    Key key,
+    @required this.name,
+  }) : super(key: key);
+
+  final String name;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(right: 5),
+      child: Badge(
+        elevation: 0,
+        toAnimate: false,
+        shape: BadgeShape.square,
+        badgeColor: Colors.green,
+        borderRadius: BorderRadius.circular(50),
+        badgeContent: Text(
+          name,
+          style: const TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.w500,
+            fontSize: 12,
+          ),
+        ),
       ),
     );
   }
